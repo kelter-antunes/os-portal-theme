1c73c7035e6dfedb3d7b3ddff1b151a872d64e13
kelter <miguel.aka.kelter@gmail.com>
2015-08-17 10:58:33 +0100

    1st

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ To develop, you'll need to first install dependencies
 $ npm install
 
 ```
+
+After developing, build the package
+
+```
+$ grunt
+
+```
+
--- gruntfile.js
+++ gruntfile.js
@@ -9,25 +9,51 @@ module.exports = function(grunt) {
                     banner: '/* My minified css file */'
                 },
                 files: {
-                    'bundle/site.css': [
-                        'src/os-font.min.css',
-                        'src/bootstrap-base.min.css',
-                        'src/website.min.css',
-                        'src/website-header.min.css',
-                        'src/website-footer.min.css'
+                    'css/bundle/os-portal-theme.css': [
+                        'css/src/bootstrap.css',
+                        'css/src/community-base-theme.css'
+                    ]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                compress: true
+            },
+            build: {
+                files: {
+                    'bundle/core.js': [
+                        'src/jquery-1.10.2.min.js',
+                        'src/jQueryMobile1_3_1_Custom.min.js',
+                        'src/jquery.easing.1.3.min.js',
+                        'src/jquery.animate-enhanced.min.js'
                     ],
-                    'bundle/site-responsive.css': [
-                        'src/os-font.min.css',
-                        'src/bootstrap-base-responsive.min.css',
-                        'src/website.min.css',
-                        'src/website-header.min.css',
-                        'src/website-responsive.min.css',
-                        'src/website-footer-responsive.min.css'
+                    'bundle/extra.js': [
+                        'src/jquery.lazyloadxt.extra.min.js',
+                        'src/common.min.js',
+                        'src/os-inspectlet.min.js',
+                        'src/jquery.panelslider.min.js',
+                        'src/home-parallax.min.js',
+                        'src/jquery.powertip.min.js',
+                        'src/os-ws-popup.min.js',
+                        'src/carousel.min.js',
+                        'src/jquery.sticky.min.js',
+                        'src/jquery.actual.min.js',
+                        'src/isotope.pkgd.min.js',
+                        'src/jquery.magnific-popup.min.js',
+                        'src/imagelightbox.min.js',
+                        'src/jquery.ba-hashchange.min.js',
+                        'src/jquery.swiftype.search.min.js',
+                        'src/jquery.swiftype.autocomplete.min.js',
+                        'src/os-swiftype.min.js',
+                        'src/jQuery.succinct.min.js',
+                        'src/jquery.zaccordion.min.js'
+
                     ]
                 }
             }
         }
     });
     // Default task.  
-    grunt.registerTask('default', ['cssmin']);
+    grunt.registerTask('default', ['cssmin', 'uglify']);
 };
